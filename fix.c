f56a4e1965d53eb0b82345b49e1199bfa1c089ba
francois141 <47925812+francois141@users.noreply.github.com>
2023-05-24 12:53:55 +0200

    Update fix.c

--- fix.c
+++ fix.c
@@ -6,3 +6,18 @@ barrelfish_usleep(1000);
 if ((tc && cc) || (cc && !is_read && !is_write)) {
   break;
 }
+
+// Code to test the fix
+for(int k = 0; k < 10;k++) {
+  for(int i = 2; i <= 30;i++) {
+    debug_printf("Write read block : %d\n", i);
+    for(int j = 0; j < 512;j++) {
+      block[j] = (j + i) % 199;
+    }
+    assert(err_is_ok(write_block(b_driver, i, block)));
+    assert(err_is_ok(read_block(b_driver, i, block2)));
+    for(int j = 0; j < 512;j++) {
+      assert(block[j] == block2[j]);
+    }
+  }
+}
